d5ff50f3c888030cfe1b3c2e626b481b8cf84339
Adercio <adercio.macedo@4linux.com.br>
2015-12-04 01:35:14 -0200

    Criado o layout do aplicativo

---
+++ app/src/main/java/br/com/devops/restaurantregistration/RestaurantListActivity.java
@@ -0,0 +1,22 @@
+package br.com.devops.restaurantregistration;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class RestaurantListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
